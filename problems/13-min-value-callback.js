5d35c81efe0dcd4b543386ce706c9cccd315e64a
Saroj Shrestha <sarojshrestha@Sarojs-iMac.local>
2023-08-14 17:06:59 -0600

    new commit

---
+++ problems/13-min-value-callback.js
@@ -0,0 +1,34 @@
+/*******************************************************************************
+Write a function `minValueCallback` that accepts an array and an optional callback as arguments.
+If a callback is not passed in, then the function should return the smallest
+value in the array. If a callback is passed in, then the function should return
+the result of the smallest value being passed into the given callback.
+
+Examples:
+console.log(minValueCallback([64, 25, 49, 9, 100]));             // 9
+console.log(minValueCallback([64, 25, 49, 9, 100], Math.sqrt));  // 3
+
+*******************************************************************************/
+
+let minValueCallback = function(arr, cb) {
+    let lowestNumber = arr[0];
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] < lowestNumber) {
+            lowestNumber = arr[i];
+        }
+    }
+    if(cb === undefined){
+        return lowestNumber;
+    } else {
+        return cb(lowestNumber);
+    }
+    
+};
+
+
+
+
+
+
+/*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
+module.exports = minValueCallback;
---
+++ problems/16-suffix-cipher.js
@@ -0,0 +1,58 @@
+/*******************************************************************************
+Write a function `suffixCipher` that accepts a sentence and object as arguments.
+The object contains suffixes as keys and callbacks as values. The `suffixCipher`
+function should return a new sentence where words of the original sentence are
+modified according to the callback that corresponds with the suffix that the word
+ends with. If the word does not end in any of the suffix keys, then it should not
+be modified. You can assume that only one suffix of the object will match a word.
+
+Examples:
+
+let cipher1 = {
+    ly: function(word) {
+        return word.slice(0, -1) + 'ee';
+    },
+    ize: function(word) {
+        return word + 'r';
+    }
+};
+console.log(suffixCipher('quietly and gently visualize', cipher1));
+// quietlee and gentlee visualizer
+
+let cipher2 = {
+    tal: function(word) {
+        return word.toUpperCase();
+    },
+    s: function(word) {
+        return word + 'th';
+    }
+};
+console.log(suffixCipher('incremental progress is very instrumental', cipher2));
+// INCREMENTAL progressth isth very INSTRUMENTAL
+*******************************************************************************/
+
+let suffixCipher = function(sentence, obj) {
+    sentence = sentence.split(' ');
+    let cipherstring = [];
+    for(word of sentence) {
+        let stat = false;
+        for(key in obj){
+            if(word.endsWith(key)){
+                cipherstring.push(obj[key](word));
+                stat = true;
+            }
+        }
+        if(stat === false){
+            cipherstring.push(word);
+        }
+    }
+    return cipherstring.join(' ');
+};
+
+
+
+
+
+
+/*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
+module.exports = suffixCipher;
